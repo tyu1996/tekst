f968dbe2a8362006096cc85b28ca8351cb2ab6dc
Brian <tingsy@sains.com.my>
2021-01-27 15:51:11 +0800

    [Feature] String insertion & deletion

--- src/row.rs
+++ src/row.rs
@@ -1,6 +1,7 @@
 use std::cmp;
 use unicode_segmentation::UnicodeSegmentation;
 
+#[derive(Default)]
 pub struct Row {
     string: String,
     len: usize,
@@ -44,5 +45,31 @@ impl Row {
     }
     fn update_len(&mut self) {
         self.len = self.string[..].graphemes(true).count();
+    }
+    pub fn insert(&mut self, at: usize, c: char) {
+        if at >= self.len() {
+            // If at the end of line, just push new string
+            self.string.push(c);
+        } else {
+            // If in the middle of line, push new string
+            // from the cursor position, then glue remaining
+            // strings afterwards
+            let mut result: String = self.string[..].graphemes(true).take(at).collect();
+            let remainder: String = self.string[..].graphemes(true).skip(at).collect();
+            result.push(c);
+            result.push_str(&remainder);
+            self.string = result;
+        }
+        self.update_len();
+    }
+    pub fn delete(&mut self, at: usize) {
+        if at >= self.len() {
+            return;
+        }
+        let mut result: String = self.string[..].graphemes(true).take(at).collect();
+        let remainder: String = self.string[..].graphemes(true).skip(at + 1).collect();
+        result.push_str(&remainder);
+        self.string = result;
+        self.update_len();
     }
 }
